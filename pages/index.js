6aefa57c7a671a4cee98c87603ea56efcb179964
Alex <alexrfarnes@gmail.com>
2021-08-26 15:49:37 +0800

    Style individual product page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React from "react";
 import ProductList from "../components/Index/ProductList";
+import baseUrl from "../utils/baseUrl";
 
 function Home({ products }) {
   return <ProductList products={products} />;
@@ -9,7 +10,7 @@ function Home({ products }) {
 // Instead of fetching data after the Component is mounted (client side) like with React.useEffect(), getInitialProps allows to perform the side effect (fetch) on the server side
 Home.getInitialProps = async () => {
   // fetch data on server
-  const url = "http://localhost:3000/api/products";
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   // return response data as an object
   // note: this object will be merged with existing props
