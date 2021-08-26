e202b2ed51680ac6ffa2807bf1c6dc38f1e9d9ad
Alex <alexrfarnes@gmail.com>
2021-08-26 18:55:00 +0800

    Handle errors on client and server side

--- pages/create.js
+++ pages/create.js
@@ -11,6 +11,7 @@ import {
 } from "semantic-ui-react";
 import axios from "axios";
 import baseUrl from "../utils/baseUrl";
+import catchErrors from "../utils/catchErrors";
 
 const INITIAL_PRODUCT = {
   name: "",
@@ -24,6 +25,14 @@ function CreateProduct() {
   const [mediaPreview, setMediaPreview] = React.useState("");
   const [success, setSuccess] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
+  const [disabled, setDisabled] = React.useState(true);
+  const [error, setError] = React.useState("");
+
+  // Check if every product field has been filled
+  React.useEffect(() => {
+    const isProduct = Object.values(product).every(el => Boolean(el));
+    isProduct ? setDisabled(false) : setDisabled(true);
+  }, [product]);
 
   function handleChange(event) {
     const { name, value, files } = event.target;
@@ -46,17 +55,21 @@ function CreateProduct() {
   }
 
   async function handleSubmit(event) {
-    event.preventDefault();
-    setLoading(true);
-    const mediaUrl = await handleImageUpload();
-    const url = `${baseUrl}/api/product`;
-    const { name, price, description } = product;
-    const payload = { name, price, description, mediaUrl };
-    const response = await axios.post(url, payload);
-    console.log({ response });
-    setLoading(false);
-    setProduct(INITIAL_PRODUCT);
-    setSuccess(true);
+    try {
+      event.preventDefault();
+      setLoading(true);
+      const mediaUrl = await handleImageUpload();
+      const url = `${baseUrl}/api/product`;
+      const { name, price, description } = product;
+      const payload = { name, price, description, mediaUrl };
+      const response = await axios.post(url, payload);
+      setProduct(INITIAL_PRODUCT);
+      setSuccess(true);
+    } catch (error) {
+      catchErrors(error, setError);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -64,7 +77,12 @@ function CreateProduct() {
       <Header as='h2' block>
         <Icon name='add' color='orange' /> Create New Product
       </Header>
-      <Form loading={loading} success={success} onSubmit={handleSubmit}>
+      <Form
+        loading={loading}
+        success={success}
+        error={Boolean(error)}
+        onSubmit={handleSubmit}>
+        <Message error header='Oops!' content={error} />
         <Message
           success
           icon='check'
@@ -112,7 +130,7 @@ function CreateProduct() {
         />
         <Form.Field
           control={Button}
-          disabled={loading}
+          disabled={disabled || loading}
           color='blue'
           icon='pencil alternate'
           content='Submit'
