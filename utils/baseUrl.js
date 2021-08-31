26868b1f87ac26e539556655d1d8caa0de80575b
Alex <alexrfarnes@gmail.com>
2021-08-31 17:23:56 +0800

    update baseUrl

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://deployment-url.now.sh"
+    ? "https://react-commerce-mu.vercel.app"
     : "http://localhost:3000";
 
 export default baseUrl;
